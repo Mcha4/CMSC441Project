c00cef9f8d1ae3ccff585d7535936dc4c7bc6e4d
mcha4 <mcha4@umbc.edu>
2022-03-11 11:23:33 -0500

    Push

--- project.cpp
+++ project.cpp
@@ -55,6 +55,7 @@ int** addMatrix(int** A, int** B, int matrixSize){
   return result;
 }
 
+//Subtract two matrices and return result
 int** subMatrix(int** A, int** B, int matrixSize){
   int** result = new int*[matrixSize];
   for(int i = 0; i < matrixSize; i++){
@@ -68,6 +69,7 @@ int** subMatrix(int** A, int** B, int matrixSize){
   return result;
 }
 
+//Doesn't need but IDK
 void split(int** parent, int** child, int from, int to, int matrixSize){
   for(int i1 = 0, i2 = from; i1 < matrixSize; i1++, i2++){
     for(int j1 = 0, j2 = to; j1 < matrixSize; j1++, j2++){
@@ -76,6 +78,7 @@ void split(int** parent, int** child, int from, int to, int matrixSize){
   }
 }
 
+//Insert result into the parent for Starssen
 void insertResult(int** parent, int** child, int from, int to, int matrixSize){
   for(int i1 = 0, i2 = from; i1 < matrixSize; i1++, i2++){
     for(int j1 = 0, j2 = to; j1 < matrixSize; j1++, j2++){
@@ -84,121 +87,140 @@ void insertResult(int** parent, int** child, int from, int to, int matrixSize){
   }
 }
 
+//print givenMatrix
+void printMatrix(int** matrix, int matrixSize){
+  cout << "Matrix" << endl << endl;
+  for(int i = 0; i < matrixSize; i++){
+    for(int j = 0; j < matrixSize; j++){
+      cout << matrix[i][j] << " ";
+    }
+    cout << endl << endl;
+  }
+}
+
 //StrassenMatrix Multiplication
 void StrassenMatrix(int** A, int** B, int** result, int matrixSize){
-  
-  //Divide 
-  //Initalize
-  int** A11 = new int*[matrixSize/2];
-  int** A12 = new int*[matrixSize/2];
-  int** A21 = new int*[matrixSize/2];
-  int** A22 = new int*[matrixSize/2];
-  int** B11 = new int*[matrixSize/2];
-  int** B12 = new int*[matrixSize/2];
-  int** B21 = new int*[matrixSize/2];
-  int** B22 = new int*[matrixSize/2];
-  for(int i = 0; i < matrixSize/2; i++){
-    A11[i] = new int[matrixSize/2];
-    A12[i] = new int[matrixSize/2];
-    A21[i] = new int[matrixSize/2];
-    A22[i] = new int[matrixSize/2];
-    B11[i] = new int[matrixSize/2];
-    B12[i] = new int[matrixSize/2];
-    B21[i] = new int[matrixSize/2];
-    B22[i] = new int[matrixSize/2];
-  }
-  cout << "Where" << endl;
-  if(matrixSize >= 2){
-    for(int i = 0; i <matrixSize/2; i++){
-      for(int j = 0; j <matrixSize/2; j++) {
-          A11[i][j] = A[i][j];
-          A12[i][j] = A[i][matrixSize/2+j];
-          A21[i][j] = A[matrixSize/2+i][j];
-          A22[i][j] = A[matrixSize/2+i][matrixSize/2+j];
-          B11[i][j] = B[i][j];
-          B12[i][j] = B[i][matrixSize/2+j];
-          B21[i][j] = B[matrixSize/2+i][j];
-          B22[i][j] = B[matrixSize/2+i][matrixSize/2+j];
+
+  if(A[0][0] != 0 && B[0][0] != 0){
+    printMatrix(A, matrixSize);
+    printMatrix(B, matrixSize);
+    //Divide 
+    //Initalize
+    int** A11 = new int*[matrixSize/2];
+    int** A12 = new int*[matrixSize/2];
+    int** A21 = new int*[matrixSize/2];
+    int** A22 = new int*[matrixSize/2];
+    int** B11 = new int*[matrixSize/2];
+    int** B12 = new int*[matrixSize/2];
+    int** B21 = new int*[matrixSize/2];
+    int** B22 = new int*[matrixSize/2];
+    for(int i = 0; i < matrixSize/2; i++){
+      A11[i] = new int[matrixSize/2];
+      A12[i] = new int[matrixSize/2];
+      A21[i] = new int[matrixSize/2];
+      A22[i] = new int[matrixSize/2];
+      B11[i] = new int[matrixSize/2];
+      B12[i] = new int[matrixSize/2];
+      B21[i] = new int[matrixSize/2];
+      B22[i] = new int[matrixSize/2];
+    }
+    cout << "Where" << endl;
+    if(matrixSize >= 2){
+      for(int i = 0; i < matrixSize/2; i++){
+        for(int j = 0; j < matrixSize/2; j++) {
+            A11[i][j] = A[i][j];
+            A12[i][j] = A[i][matrixSize/2+j];
+            A21[i][j] = A[matrixSize/2+i][j];
+            A22[i][j] = A[matrixSize/2+i][matrixSize/2+j];
+            B11[i][j] = B[i][j];
+            B12[i][j] = B[i][matrixSize/2+j];
+            B21[i][j] = B[matrixSize/2+i][j];
+            B22[i][j] = B[matrixSize/2+i][matrixSize/2+j];
+        }
       }
     }
+    //M1 = (A11 + A22) * (B11 + B22)
+    int** M1 = new int*[matrixSize/2];
+    for(int i = 0; i < matrixSize/2; i++){
+      M1[i] = new int[matrixSize/2];
+    }
+    StrassenMatrix((addMatrix(A11, A22, matrixSize/2)), addMatrix(B11, B22, matrixSize/2), M1, matrixSize/2);
+    printMatrix(M1, matrixSize/2);
+
+    for(int i = 0; i < matrixSize/2; i++){
+      delete[] A11[i];
+      delete[] A12[i];
+      delete[] A21[i];
+      delete[] A22[i];
+      delete[] B11[i];
+      delete[] B12[i];
+      delete[] B21[i];
+      delete[] B22[i];
+      delete[] M1[i];
+      // delete[] M2[i];
+      // delete[] M3[i];
+      // delete[] M4[i];
+      // delete[] M5[i];
+      // delete[] M6[i];
+      // delete[] M7[i];
+    }
+    delete[] A11;
+    delete[] A12;
+    delete[] A21;
+    delete[] A22;
+    delete[] B11;
+    delete[] B12;
+    delete[] B21;
+    delete[] B22;
+    delete[] M1;
+    // delete[] M2;
+    // delete[] M3;
+    // delete[] M4;
+    // delete[] M5;
+    // delete[] M6;
+    // delete[] M7;
   }
   
-  
-  //M1 = (A11 + A22) * (B11 + B22)
-  int** M1 = new int*[matrixSize];
-  StrassenMatrix((addMatrix(A11, A22, matrixSize/2)), addMatrix(B11, B22, matrixSize/2), M1, matrixSize/2);
 
-  //M2 = (A21 + A22) * B11
-  int** M2 = new int*[matrixSize];
-  StrassenMatrix((addMatrix(A21, A22, matrixSize/2)), B11, M2, matrixSize/2);
+  // //M2 = (A21 + A22) * B11
+  // int** M2 = new int*[matrixSize];
+  // StrassenMatrix((addMatrix(A21, A22, matrixSize/2)), B11, M2, matrixSize/2);
 
-  //M3 = A11 * (B12-B22)
-  int** M3 = new int*[matrixSize];
-  StrassenMatrix(A11, subMatrix(B11, B22, matrixSize/2), M3, matrixSize/2);
+  // //M3 = A11 * (B12-B22)
+  // int** M3 = new int*[matrixSize];
+  // StrassenMatrix(A11, subMatrix(B11, B22, matrixSize/2), M3, matrixSize/2);
 
-  //M4 = A22 * (B21 - B11)
-  int** M4 = new int*[matrixSize];
-  StrassenMatrix(A22, addMatrix(B21, B11, matrixSize/2), M4, matrixSize/2);
+  // //M4 = A22 * (B21 - B11)
+  // int** M4 = new int*[matrixSize];
+  // StrassenMatrix(A22, addMatrix(B21, B11, matrixSize/2), M4, matrixSize/2);
 
-  //M5 = (A11 + A12) * B22
-  int** M5 = new int*[matrixSize];
-  StrassenMatrix(addMatrix(A11, A12, matrixSize/2), B22, M5, matrixSize/2);
+  // //M5 = (A11 + A12) * B22
+  // int** M5 = new int*[matrixSize];
+  // StrassenMatrix(addMatrix(A11, A12, matrixSize/2), B22, M5, matrixSize/2);
 
-  //M6 = (A21- A11) * (B11 + B12)
-  int** M6 = new int*[matrixSize];
-  StrassenMatrix((subMatrix(A21, A11, matrixSize/2)), addMatrix(B11, B12, matrixSize/2), M6, matrixSize/2);
+  // //M6 = (A21- A11) * (B11 + B12)
+  // int** M6 = new int*[matrixSize];
+  // StrassenMatrix((subMatrix(A21, A11, matrixSize/2)), addMatrix(B11, B12, matrixSize/2), M6, matrixSize/2);
 
-  //M7 = (A12 - A22) * (B21 + B22)
-  int** M7 = new int*[matrixSize];
-  StrassenMatrix(subMatrix(A12, A22, matrixSize/2), addMatrix(B21, B22, matrixSize/2), M7, matrixSize/2);
+  // //M7 = (A12 - A22) * (B21 + B22)
+  // int** M7 = new int*[matrixSize];
+  // StrassenMatrix(subMatrix(A12, A22, matrixSize/2), addMatrix(B21, B22, matrixSize/2), M7, matrixSize/2);
 
-
-  //C11 = M1 + M4 - M5 + M7
-  //C12 = M3 + M5
-  //C21 = M2 + M4
-  //C22 = M1 - M2 + M3 + M6
-  int** C11 = subMatrix(addMatrix(M1, M4, matrixSize/2), addMatrix(M5, M7, matrixSize/2), matrixSize/2);
-  int** C12 = addMatrix(M3, M5, matrixSize/2);
-  int** C21 = addMatrix(M2, M4, matrixSize/2);
-  int** C22 = addMatrix(subMatrix(M1, M2, matrixSize/2), addMatrix(M3, M6, matrixSize/2), matrixSize/2);
-  insertResult(result, C11, 0, matrixSize/2, matrixSize/2);
-  insertResult(result, C12, 0, matrixSize/2, matrixSize/2);
-  insertResult(result, C21, 0, matrixSize/2, matrixSize/2);
-  insertResult(result, C22, 0, matrixSize/2, matrixSize/2);
+  // //C11 = M1 + M4 - M5 + M7
+  // //C12 = M3 + M5
+  // //C21 = M2 + M4
+  // //C22 = M1 - M2 + M3 + M6
+  // int** C11 = subMatrix(addMatrix(M1, M4, matrixSize/2), addMatrix(M5, M7, matrixSize/2), matrixSize/2);
+  // int** C12 = addMatrix(M3, M5, matrixSize/2);
+  // int** C21 = addMatrix(M2, M4, matrixSize/2);
+  // int** C22 = addMatrix(subMatrix(M1, M2, matrixSize/2), addMatrix(M3, M6, matrixSize/2), matrixSize/2);
+  // insertResult(result, C11, 0, matrixSize/2, matrixSize/2);
+  // insertResult(result, C12, 0, matrixSize/2, matrixSize/2);
+  // insertResult(result, C21, 0, matrixSize/2, matrixSize/2);
+  // insertResult(result, C22, 0, matrixSize/2, matrixSize/2);
 
   //Delete pointers
-  for(int i = 0; i < matrixSize/2; i++){
-    delete[] A11[i];
-    delete[] A12[i];
-    delete[] A21[i];
-    delete[] A22[i];
-    delete[] A11[i];
-    delete[] A12[i];
-    delete[] A21[i];
-    delete[] A22[i];
-    delete[] M1[i];
-    delete[] M2[i];
-    delete[] M3[i];
-    delete[] M4[i];
-    delete[] M5[i];
-    delete[] M6[i];
-    delete[] M7[i];
-  }
-  delete[] A11;
-  delete[] A12;
-  delete[] A21;
-  delete[] A22;
-  delete[] A11;
-  delete[] A12;
-  delete[] A21;
-  delete[] A22;
-  delete[] M1;
-  delete[] M2;
-  delete[] M3;
-  delete[] M4;
-  delete[] M5;
-  delete[] M6;
-  delete[] M7;
+  
 }
 
 //Print data in table
@@ -221,7 +243,7 @@ int main() {
 
 
   //Iteration to make data in different sizes;
-  for(int count = 0; count < 1; count++){
+  for(int count = 0; count < 2; count++){
     //increase the size
     matrixSize = pow(2, count+1);
 
